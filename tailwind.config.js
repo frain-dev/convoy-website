461b372b3eb6fbe14839818afc4dc9ee365d13e0
Pelumi Honey <pelumioni25@gmail.com>
2022-08-23 19:48:42 +0100

    uodated css and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,32 +18,33 @@ function customSpacing() {
 module.exports = {
 	mode: 'jit',
 	content: ['./components/**/*.{js,vue,ts}', './layouts/**/*.vue', './pages/**/*.vue', './plugins/**/*.{js,ts}', './nuxt.config.{js,ts}'],
-	safelist: ['bg-success-500', 'text-success-100', 'bg-warning-500', 'text-warning-100', 'bg-danger-500', 'text-danger-100', 'text-grey-40', 'bg-grey-10', 'bg-primary-400', 'bg-danger-100'],
 	theme: {
 		extend: {
 			spacing: customSpacing(),
 			boxShadow: {
 				sm: '0px 2px 8px rgba(12, 26, 75, 0.08), 0px 3px 8px -1px rgba(50, 50, 71, 0.05)',
 				default: '0px 2px 4px rgba(12, 26, 75, 0.04), 0px 4px 20px -2px rgba(50, 50, 71, 0.08)',
-				lg: '0px 4px 8px rgba(12, 26, 75, 0.1), 0px 10px 16px rgba(20, 37, 63, 0.06)',
-				xl: '0px 8px 16px rgba(12, 26, 75, 0.1), 0px 20px 24px rgba(20, 37, 63, 0.06)',
-				'2xl': '0px 16px 16px rgba(12, 26, 75, 0.05), 0px 30px 40px rgba(20, 37, 63, 0.08)'
+				card: '40px 44px 81px rgba(111, 118, 138, 0.08)'
 			},
 			fontFamily: {
-				sans: ['Quicksand', ...defaultTheme.fontFamily.sans]
+				menlo: ['Menlo Regular', ...defaultTheme.fontFamily.sans]
 			}
 		},
 		screens: {
 			desktop: { min: '880px' },
-			mobile: { max: '875px' },
+			md: { min: '1240px' },
+			mobile: { max: '879px' }
 		},
 		borderRadius: {
+			'2px': '2px',
 			'4px': '4px',
+			'6px': '6px',
 			'8px': '8px',
 			'10px': '10px',
 			'12px': '12px',
 			'16px': '16px',
-			'100px': '100px'
+			'100px': '100px',
+			'50%': '50%'
 		},
 		fontSize: {
 			10: ['10px', '150%'],
@@ -53,10 +54,15 @@ module.exports = {
 			18: ['18px', '30px'],
 			20: ['20px', '30px'],
 			24: ['24px', '35px'],
-			h1: ['24px', '140%'],
-			h2: ['18px', '140%'],
-			h3: ['16px', '140%'],
-			h4: ['14px', '140%']
+			26: ['26px', '36px'],
+			28: ['28px', '38px'],
+			30: ['30px', '46px'],
+			h1: ['34px', '140%'],
+			h2: ['24px', '140%'],
+			h3: ['20px', '140%'],
+			h4: ['18px', '140%'],
+			h5: ['16px', '140%'],
+			h6: ['14px', '140%']
 		},
 		colors: {
 			grey: {
@@ -64,7 +70,7 @@ module.exports = {
 				80: '#31323D',
 				60: '#5F5F68',
 				40: '#737A91',
-				20: '#E8E8E9',
+				20: '#f3f3f8',
 				10: '#EDEDF5'
 			},
 			white: {
@@ -73,6 +79,7 @@ module.exports = {
 				40: 'rgba(var(--color-white), 0.40)',
 				24: 'rgba(var(--color-white), 0.24)',
 				16: 'rgba(var(--color-white), 0.16)',
+				10: 'rgba(var(--color-white), 0.1)',
 				8: 'rgba(var(--color-white), 0.08)',
 				4: 'rgba(var(--color-white), 0.04)'
 			},
