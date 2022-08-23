423a331e04d2e6d2ff3f06400bc04d324db0e1a5
Pelumi Honey <pelumioni25@gmail.com>
2022-08-23 19:47:40 +0100

    updated footer component

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,10 +1,10 @@
 <template>
-	<footer>
-		<div class="container">
-			<nav>
+	<footer class="bg-[#1e252b] text-white-100 pt-40px pb-32px px-20px">
+		<div class="max-w-[1200px] m-auto">
+			<nav class="flex justify-between items-center flex-wrap pb-42px border-b border-b-white-10">
 				<div>
-					<div class="logo">
-						<img src="~/assets/images/logo.svg" alt="logo" />
+					<div class="mb-78px">
+						<img src="~/assets/images/logo.svg" class="h-28px w-110px" alt="logo" />
 					</div>
 					<ul class="socials">
 						<li>
@@ -26,24 +26,24 @@
 						</li>
 					</ul>
 				</div>
-				<div class="newsletter">
-					<div>
-						<div>
-							<h5>Join our newsletter</h5>
-							<p>No spam! Just articles, events, and talks.</p>
+				<div class="w-full p-0 desktop:max-w-[430px] desktop:flex-row desktop:justify-around desktop:items-center">
+					<div class="flex justify-between items-center w-full m-0">
+						<div class="order-1">
+							<h6 class="text-left">Join our newsletter</h6>
+							<p class="text-14 text-left mt-24px">No spam! Just articles, events, and talks.</p>
 						</div>
-						<img src="~/assets/images/mailbox.gif" alt="mailbox animation" />
+						<img src="~/assets/images/mailbox.gif" class="w-124px order-2" alt="mailbox animation" />
 					</div>
-					<form @submit.prevent="requestAccess()">
-						<img src="~/assets/images/mail-primary-icon.svg" alt="mail icon" />
-						<input type="email" id="email" placeholder="Your email" aria-label="Email" v-model="earlyAccessEmail" />
-						<button>
+					<form class="bg-[#1c2126] border border-[#262f37] rounded-8px -mt-10px p-10px flex" @submit.prevent="requestAccess()">
+						<img src="~/assets/images/mail-primary-icon.svg" class="w-32px" alt="mail icon" />
+						<input type="email" id="email" placeholder="Your email" aria-label="Email" v-model="earlyAccessEmail" class="w-full bg-transparent border-none outline-none text-white-100 placeholder:text-white-100" />
+						<button class="text-16 font-semibold bg-transparent border-none">
 							<img src="~/assets/images/send-primary-icon.svg" alt="send icon" />
 						</button>
 					</form>
 				</div>
 			</nav>
-			<p class="copyright">Copyright {{ currentYear }}, All Rights Reserved</p>
+			<p class="mt-24px text-left desktop:text-right">Copyright {{ currentYear }}, All Rights Reserved</p>
 		</div>
 	</footer>
 </template>
@@ -91,139 +91,3 @@ export default {
 	}
 };
 </script>
-<style lang="scss" scoped>
-$desktopBreakPoint: 880px;
-.newsletter {
-	display: unset;
-	width: 100%;
-	padding: 0;
-	margin-top: 41px;
-
-	@media (min-width: $desktopBreakPoint) {
-		max-width: 430px;
-	}
-
-	h5,
-	p {
-		text-align: left;
-	}
-
-	& > div {
-		display: flex;
-		justify-content: space-between;
-		margin: 0;
-		width: 100%;
-		max-width: unset;
-		align-items: center;
-
-		div {
-			order: 1;
-			max-width: unset;
-			margin: unset;
-		}
-
-		img {
-			order: 2;
-			width: 125px;
-		}
-	}
-
-	form {
-		background: #1c2126;
-		border: 1px solid #262f37;
-		box-sizing: border-box;
-		border-radius: 8px;
-		margin-top: -10px;
-	}
-input{
-	color: #fff;
-}
-	input::placeholder {
-		color: #ebf4f1;
-		opacity: 1;
-	}
-}
-footer {
-	.container {
-		max-width: 1170px;
-	}
-	nav {
-		display: flex;
-		justify-content: space-between;
-		flex-wrap: wrap;
-	}
-	.logo {
-		height: 28px;
-		margin-bottom: 78px;
-		img {
-			height: 28px;
-			width: 109px;
-		}
-	}
-	.join-news-letter {
-		display: flex;
-		align-items: flex-start;
-		@media (max-width: 425px) {
-			margin-top: 41px;
-		}
-		p {
-			font-weight: 500;
-			font-size: 14px;
-			line-height: 22px;
-			color: #ebf4f1;
-			margin-bottom: 10px;
-			margin-top: unset;
-			text-align: left;
-		}
-		img {
-			height: 125px;
-		}
-	}
-	.input {
-		margin-top: -25px;
-		max-width: 460px;
-		width: 100%;
-		display: flex;
-		align-items: center;
-		position: relative;
-		input {
-			background: #1c2126;
-			border: 1px solid #262f37;
-			box-sizing: border-box;
-			border-radius: 8px;
-			height: 56px;
-			width: 100%;
-			padding-left: 40px;
-			font-size: 16px;
-			line-height: 180%;
-			color: #fff;
-			&::placeholder {
-				color: #ebf4f1;
-			}
-			&:focus {
-				outline: none;
-				box-shadow: none;
-				border: 1px solid #477db3;
-				transition: 0.3 ease-in-out;
-			}
-		}
-
-		.prepend-icon {
-			height: 12px;
-			width: 16px;
-			margin-right: -30px;
-			z-index: 11;
-		}
-
-		button {
-			margin-left: -50px;
-		}
-	}
-	p.copyright {
-		margin-top: 24px;
-		@media (max-width: 425px) {
-			text-align: left;
-		}
-	}
-}
-</style>
