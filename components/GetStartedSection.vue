57a59f8eaf052b92b50a2f55db63e7358bbe006c
Emmanuel Aina <emmanuel.ainaj@gmail.com>
2023-01-14 01:33:56 +0000

    update text in getting started section

--- components/GetStartedSection.vue
+++ components/GetStartedSection.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="px-20px pb-100px desktop:pb-130px">
         <div class="mt-100px desktop:mt-130px max-w-[1000px] w-full m-auto bg-[url(~/assets/images/cta.png)] bg-no-repeat bg-cover bg-top bg-blend-normal bg-[#422F41] rounded-16px py-56px px-20px">
-            <h1 class="text-32 desktop:text-[40px] text-white-100 font-bold tracking-[0.02em] text-center mb-20px desktop:mb-16px">Start sending webhooks now, risk free</h1>
+            <h1 class="text-32 desktop:text-[40px] text-white-100 font-bold tracking-[0.02em] text-center mb-20px max-w-[562px] m-auto">Start sending and receiving webhooks now, risk free</h1>
             <p class="text-center text-14 desktop:text-18 text-white-100 max-w-[806px] m-auto">
                 Convoy provides you with fast, secure and reliable webhooks infrastructure so you can focus on building the actual tech. Save yourself some engineering time and get started today.
             </p>
