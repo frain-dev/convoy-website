6b39ed29fde69d13f144dd23893dac5edced5832
Pelumi Honey <pelumioni25@gmail.com>
2023-02-07 13:15:14 +0100

    update: update header component

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,25 @@
 <template>
 	<header>
-		<nav class="w-full m-auto px-20px pt-60px pb-20px z-50 fixed left-[50%] -translate-x-1/2 translate-y-0 nav-bar-break:pt-50px nav-bar-break:pb-12px transition-all duration-300 bg-white-100 backdrop-blur-[36]">
+		<nav
+			class="
+				w-full
+				m-auto
+				px-20px
+				pt-60px
+				pb-20px
+				z-50
+				fixed
+				left-[50%]
+				-translate-x-1/2
+				translate-y-0
+				nav-bar-break:pt-50px nav-bar-break:pb-12px
+				transition-all
+				duration-300
+				bg-white-100
+				shadow-[inset_0px_-3px_8px_rgba(255,255,255,0.07)]
+				backdrop-blur-[36]
+			"
+		>
 			<section class="fixed top-0 left-0 bg-primary-100 w-full h-40px py-8px px-12px flex items-center justify-center font-medium text-12 text-white-100 nav-bar-break:text-14">
 				<span>Give us a star on GitHub</span>
 				<a class="h-20px w-20px mx-12px hover:cursor-pointer" target="_blank" rel="noopener noreferrer" href="https://github.com/frain-dev/convoy">
@@ -26,19 +45,35 @@
 					</nuxt-link>
 				</div>
 
-				<ul class="
+				<ul
+					class="
 						mobile:absolute mobile:top-104px mobile:left-20px mobile:text-left mobile:bg-white-100 mobile:shadow-sm mobile:rounded-10px mobile:min-w-[250px]
 						nav-bar-break:flex nav-bar-break:items-center nav-bar-break:justify-end nav-bar-break:bg-transparent
 						transition-all
 						duration-500
-					" :class="showMenu ? 'mobile:h-fit mobile:block mobile:z-50' : 'mobile:hidden mobile:h-0'">
+					"
+					:class="showMenu ? 'mobile:h-fit mobile:block mobile:z-50' : 'mobile:hidden mobile:h-0'"
+				>
 					<li class="py-14px px-20px nav-bar-break:mr-20px border-b border-b-grey-10 last-of-type:border-none nav-bar-break:border-none" v-for="link in menuItems" :key="link.name">
-						<nuxt-link class=" text-14" v-if="link.type === 'route'" :to="link.route">{{ link.name }}</nuxt-link>
-						<a class=" text-14" v-else target="_blank" rel="noopener noreferrer" :href="link.route">{{ link.name }}</a>
+						<nuxt-link class="text-14" v-if="link.type === 'route'" :to="link.route">{{ link.name }}</nuxt-link>
+						<a class="text-14" v-else target="_blank" rel="noopener noreferrer" :href="link.route">{{ link.name }}</a>
 					</li>
 
 					<li class="py-14px px-20px nav-bar-break:p-0">
-						<a href="/demo" class="nav-bar-break:py-8px nav-bar-break:px-12px text-14 font-medium rounded-8px nav-bar-break:bg-primary-100 nav-bar-break:text-white-100 text-primary-100 flex items-center mr-24px">
+						<a
+							href="/demo"
+							class="
+								nav-bar-break:py-8px nav-bar-break:px-12px
+								text-14
+								font-medium
+								rounded-8px
+								nav-bar-break:bg-primary-100 nav-bar-break:text-white-100
+								text-primary-100
+								flex
+								items-center
+								mr-24px
+							"
+						>
 							Book a Demo
 						</a>
 					</li>
@@ -55,7 +90,11 @@
 			</div>
 		</nav>
 
-		<div class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-10 backdrop-blur-[25px] transition-all duration-500" :class="showMenu ? 'pointer-events-all opacity-100 z-[2]' : 'pointer-events-none opacity-0'" @click="showMenu = !showMenu"></div>
+		<div
+			class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-10 backdrop-blur-[25px] transition-all duration-500"
+			:class="showMenu ? 'pointer-events-all opacity-100 z-[2]' : 'pointer-events-none opacity-0'"
+			@click="showMenu = !showMenu"
+		></div>
 	</header>
 </template>
 
@@ -86,7 +125,7 @@ export default {
 				const response = await fetch('https://api.github.com/repos/frain-dev/convoy');
 				const data = await response.json();
 				this.githubStars = data.stargazers_count;
-			} catch (_error) { }
+			} catch (_error) {}
 		},
 		handleScroll() {
 			this.$route.name == 'blog' || window.scrollY > 50 ? (this.hasScrolled = true) : (this.hasScrolled = false);
