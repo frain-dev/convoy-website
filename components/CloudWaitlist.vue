44056aeb1e282107f16f93b92f144922276fcd3f
Pelumi Honey <pelumioni25@gmail.com>
2023-04-03 19:11:48 +0100

    update: update clound waitlist form

--- components/CloudWaitlist.vue
+++ components/CloudWaitlist.vue
@@ -1,6 +1,6 @@
 <template>
-	<div class="bg-white-100 shadow-[0px_2px_4px_rgba(12,26,75,0.04),0px_4px_20px_-2px_rgba(50,50,71,0.08)] rounded-8px p-30px desktop:pt-40px desktop:pb-70px desktop:px-60px w-full  border border-primary-200">
-		<p class="font-semibold text-18 mb-42px">Join the private waitlist</p>
+	<div class="bg-white-100 shadow-[0px_2px_4px_rgba(12,26,75,0.04),0px_4px_20px_-2px_rgba(50,50,71,0.08)] rounded-8px p-30px desktop:pt-40px desktop:pb-70px desktop:px-40px w-full border border-primary-200">
+		<p class="font-semibold text-18 mb-22px">Join the private waitlist</p>
 		<form @submit.prevent="requestAccess()">
 			<div class="grid grid-cols-2 gap-6">
 				<div>
@@ -26,6 +26,16 @@
 					/>
 				</div>
 			</div>
+
+			<label for="email" class="w-full font-medium text-12 text-grey-40 mb-8px mt-18px flex items-center justify-between">Work Email</label>
+			<input
+				id="email"
+				type="email"
+				class="transition-all duration-[.3s] w-full font-normal text-14 placeholder:text-grey-40 text-grey-100 border border-primary-500 valid:border-primary-500 disabled:border-primary-500 disabled:bg-[#F7F9FC] hover:bg-primary-500 hover:border-grey-20 focus:border-primary-100 focus:bg-white-100 outline-none rounded-4px placeholder:opacity-[.48] bg-[#F7F9FC] py-12px px-16px appearance-none"
+				v-model="requestForm.email"
+				placeholder="Johndoe@gmail.com"
+				required
+			/>
 
 			<label for="org_name" class="w-full font-medium text-12 text-grey-40 mb-8px mt-18px flex items-center justify-between">Organisation Name</label>
 			<input
@@ -61,6 +71,7 @@ export default {
 			requestForm: {
 				firstname: null,
 				lastname: null,
+				email: null,
 				usecase: null,
 				organisation: null
 			},
@@ -73,7 +84,7 @@ export default {
 			this.isSubmitingRequestAccessForm = true;
 			try {
 				const response = await fetch(
-					`https://faas-fra1-afec6ce7.doserverless.co/api/v1/web/fn-8f44e6aa-e5d6-4e31-b781-5080c050bb37/welcome-user/welcome-mail?usecase=${this.requestForm.usecase}&firstname=${this.requestForm.firstname}&lastname=${this.requestForm.lastname}&organisation=${this.requestForm.organisation}&cloud=true`
+					`https://faas-fra1-afec6ce7.doserverless.co/api/v1/web/fn-8f44e6aa-e5d6-4e31-b781-5080c050bb37/welcome-user/welcome-mail?usecase=${this.requestForm.usecase}&firstname=${this.requestForm.firstname}&lastname=${this.requestForm.lastname}&email=${this.requestForm.email}&organisation=${this.requestForm.organisation}&cloud=true`
 				);
 
 				await response.json();
