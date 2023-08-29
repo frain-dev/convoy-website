d84dc7f8319a89b76a0d4a99ae41a12ede033c58
Pelumi Honey <pelumioni25@gmail.com>
2023-08-29 18:37:24 +0100

    update: update config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 const withMarkdoc = require('@markdoc/next.js');
 
 const nextConfig = {
+	output: 'export',
 	pageExtensions: ['md', 'mdoc', 'js', 'jsx', 'ts', 'tsx'],
 	sassOptions: {
 		includePaths: [path.join(__dirname, 'styles')]
