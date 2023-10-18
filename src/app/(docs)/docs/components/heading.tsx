5ba5e3323c72651795ad05f4421881048aec178c
Pelumi Honey <pelumioni25@gmail.com>
2023-10-18 14:28:21 +0100

    update: update heading

--- src/app/(docs)/docs/components/heading.tsx
+++ src/app/(docs)/docs/components/heading.tsx
@@ -6,18 +6,18 @@ type HeadingProps = {
 export default function Heading({ children, level }: HeadingProps) {
     switch (level) {
         case 1:
-            return <h1 id={children} className="text-gray-600 text-20 font-medium pb-24px relative before:absolute before:-pt-32px before:content-[url('/doc-icons/hashtag.svg')">{children}</h1>
+            return <h1 id={children} className="text-gray-600 text-20 font-medium mb-24px relative before:absolute before:-pt-32px before:content-[url('/doc-icons/hashtag.svg')">{children}</h1>
         case 2:
-            return <h2 id={children} className="text-gray-600 text-16 font-medium pb-16px pt-40px">{children}</h2>
+            return <h2 id={children} className="text-gray-600 text-16 font-medium mb-16px mt-40px">{children}</h2>
         case 3:
-            return <h3 id={children} className="text-gray-600 text-14 font-medium pb-16px pt-40px">{children}</h3>
+            return <h3 id={children} className="text-gray-600 text-14 font-medium mb-16px mt-40px">{children}</h3>
         case 4:
-            return <h4 id={children} className="text-gray-600 text-12 font-medium pb-16px pt-40px">{children}</h4>
+            return <h4 id={children} className="text-gray-600 text-12 font-medium mb-16px mt-40px">{children}</h4>
         case 5:
-            return <h5 id={children} className="text-gray-600 text-12 font-medium pb-16px pt-40px">{children}</h5>
+            return <h5 id={children} className="text-gray-600 text-12 font-medium mb-16px mt-40px">{children}</h5>
         case 6:
-            return <h6 id={children} className="text-gray-600 text-10 font-medium pb-16px pt-40px">{children}</h6>
+            return <h6 id={children} className="text-gray-600 text-10 font-medium mb-16px mt-40px">{children}</h6>
         default:
-            <div id={children} className="text-gray-600 text-12 font-medium pb-16px pt-40px">{children}</div>
+            <div id={children} className="text-gray-600 text-12 font-medium pb-16px mt-40px">{children}</div>
     }
 }
