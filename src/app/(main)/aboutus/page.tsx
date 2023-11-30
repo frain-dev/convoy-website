5edf281cb27a993b173a696140bae1654fa7b181
Pelumi Honey <pelumioni25@gmail.com>
2023-12-01 00:33:05 +0100

    remove comments

--- src/app/(main)/aboutus/page.tsx
+++ src/app/(main)/aboutus/page.tsx
@@ -1,17 +1,5 @@
 'use client';
-import Link from 'next/link';
 import Image from 'next/image';
-import CloudImage from '../../../../public/static/cloud.png';
-import DojahIcon from '../../../../public/svg/dojah.svg';
-import CatlogIcon from '../../../../public/svg/Catlog.svg';
-import GetWalletsIcon from '../../../../public/svg/getwallets.svg';
-import PayourseIcon from '../../../../public/svg/payourse.svg';
-import PiggyvestIcon from '../../../../public/svg/piggyvest.svg';
-import TermiiIcon from '../../../../public/svg/termii.svg';
-import HailifyIcon from '../../../../public/svg/hailify-logo.svg';
-import BuycoinsIcon from '../../../../public/svg/buycoins.svg';
-import ArrowRightIcon from '../../../../public/svg/arrow-right-icon.svg';
-import Subscribe from '../../components/Subscribe';
 
 export default function AboutUs() {
 	const backedLeaders = [
