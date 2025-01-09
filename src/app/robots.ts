0b567eabc5572673bb01e7c5ae3f3b410bcf958d
Victor Williams <victor.williams@stu.cu.edu.ng>
2025-01-09 17:13:31 +0100

    perf: block cloud, community, and enterprise pages from being indexed

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: '/private/',
+      disallow: ['/cloud', '/community', '/enterprise'],
     },
     sitemap: 'https://getconvoy.io/sitemap.xml',
   }
