47789c73f3d4974e4714772f66d0eab45daffb79
Motunrayo Koyejo <koyejomotunrayo@gmail.com>
2026-03-22 18:15:14 +0100

    fix: block Googlebot from crawling Mintlify build artifacts under /docs/_next/

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
 		rules: {
 			userAgent: '*',
 			allow: '/',
-			disallow: ['/cloud', '/community', '/enterprise', '/_next/static/', '/api/', '/static/', '*/feed.rss', '*/rss']
+			disallow: ['/cloud', '/community', '/enterprise', '/_next/static/', '/docs/_next/', '/api/', '/static/', '*/feed.rss', '*/rss']
 		},
 		sitemap: 'https://www.getconvoy.io/sitemap.xml'
 	};
